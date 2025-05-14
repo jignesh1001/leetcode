c64c657a08d42c4ba1c4f11ea893c989cd014bef
jignesh@1001 <jigneshgurav12@gmail.com>
2025-05-15 02:55:55 +0530

    valid_parantheses

---
+++ ValidParantheses.js
@@ -0,0 +1,19 @@
+var isValid = function(s) {
+    const stack = []
+    const map = {
+        '(':')',
+        '{':'}',
+        '[':']'
+    }
+
+    for(let char of s){
+        if(map[char]){
+           stack.push(map[char]) 
+        }else{
+            if(stack.pop() !== char){
+                return false;
+            }
+        }
+    }
+    return stack.length === 0
+};
